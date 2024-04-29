7bf389d8ac99818ee022b63ed72e6028e27b373a
cristinasstemmer <cristinasstemmer@gmail.com>
2024-04-29 14:31:30 -0300

    alterando a estrutura das pastas

--- exercicio12.js
+++
@@ -1,5 +0,0 @@
-for (let i = 1000; i <= 1999; i++) {
-    if (i % 11 === 5) {
-        console.log(i);
-    }
-}
--- exercicio13.js
+++
@@ -1,9 +0,0 @@
-let prompt = require('prompt-sync')();
-
-for (let v = 0; v < 5; v++) {
-    let valor = parseInt(prompt(`Digite o ${v+1}º valor: `));
-    console.log(`Tabuada de ${valor}:`);
-    for (let i = 1; i <= valor; i++) {
-        console.log(`${i} x ${valor} = ${i * valor}\n`);
-}
-}
